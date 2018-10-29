375182d6fdc3c95f070114f9d74ab4c4f691022f
bqw_5189 <bqw_5189@163.com>
2018-10-29 11:31:22 +0800

    add executor pool

--- fionapet-tenant-base/src/main/java/com/fionapet/tenant/config/TaskExecutorConfig.java
+++ fionapet-tenant-base/src/main/java/com/fionapet/tenant/config/TaskExecutorConfig.java
@@ -1,6 +1,5 @@
 package com.fionapet.tenant.config;
 
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
